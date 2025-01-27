2f21cde7fe945d0f97d421189307fd34ba4048c0
Kishore kumar P <142240498+kishartz07@users.noreply.github.com>
2025-01-27 15:30:22 +0530

    Create health.ts

---
+++ health.ts
@@ -0,0 +1,15 @@
+export interface HealthData {
+  heartRate: number
+  steps: number
+  calories: number
+  sleepHours: number
+  bloodOxygen: number
+}
+
+export interface User {
+  name: string
+  age: number
+  weight: number // in kg
+  height: number // in cm
+}
+
